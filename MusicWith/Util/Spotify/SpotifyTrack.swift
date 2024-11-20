41f057bf303b7d3d603b9b5acd371c31eeb1950d
kimhappy <kimsvrkimsvr@gmail.com>
2024-11-20 12:12:27 +0900

    add: lyrics api

--- MusicWith/Util/Spotify/SpotifyTrack.swift
+++ MusicWith/Util/Spotify/SpotifyTrack.swift
@@ -9,16 +9,18 @@ import Foundation
 // https://developer.spotify.com/documentation/web-api/reference/get-track
 class SpotifyTrack {
     private var _storage: [String: Any] = [:]
-    let         trackId : String
-    let         imageURL: String? // storage 에 넣은 후 함수로 받아올 경우 무한 loading 이 걸림 => await 안 쓰게 구현시도
-    let         title   : String?
+    private var _lyric  : String?
+
+    let trackId : String
+    let imageURL: String? // storage 에 넣은 후 함수로 받아올 경우 무한 loading 이 걸림 => await 안 쓰게 구현시도
+    let title   : String?
 
     init(trackId: String, name: String? = nil, imageUrl: String? = nil, songUrl: String? = nil, lyric: String? = nil) {
         self.trackId                = trackId
+        self._lyric                 = lyric
         self._storage[ "name"     ] = name
         self._storage[ "imageUrl" ] = imageUrl
         self._storage[ "songUrl"  ] = songUrl
-        self._storage[ "lyric"    ] = lyric
         self.imageURL               = imageUrl
         self.title                  = name
     }
@@ -42,8 +44,7 @@ class SpotifyTrack {
         _storage = [
             "name"    : name    ,
             "imageUrl": imageUrl,
-            "songUrl" : songUrl ,
-            "lyric"   : "This is Lyric of the song abcdefgasdfklshdfjwe;lfj;wejfj;walfjls;jdlfaskdjfkljslkdjfkl;sjlfj;sadk;fjsdjfk;asdklfj;sldfjk;asjdfkjsofhjwiefh;wehf;iwhe;fiuheiuwfhwejafljeaokfjojsf;sdljflsdjflsjafklsdjf;lsakfjsdkfj;laskdjfklsj;oewihfiuwheiu;fniweagwiokdfjhbjwiokfjnbhjwkfhbd hjdojhbwoijfb jeojkfnh"
+            "songUrl" : songUrl
         ]
 
         return _storage[ key ]
@@ -62,6 +63,35 @@ class SpotifyTrack {
     }
 
     func lyric() async -> String? {
-        return await load(key: "lyric") as? String
+        if let lyric = _lyric {
+            return lyric
+        }
+
+        guard let url = URL(string: "http://localhost:8000/lyrics?track_id=\(trackId)") else {
+            return nil
+        }
+
+        let request = URLRequest(url: url)
+        let data    = try? await URLSession.shared.data(for: request).0
+        let json    = data.flatMap { try? JSONSerialization.jsonObject(with: $0, options: []) as? [String: Any] }
+
+        guard let json  = json,
+              let lines = json[ "lines" ] as? [[String: Any]] else {
+            return nil
+        }
+
+        var lineMerged = ""
+
+        for line in lines {
+            guard let begin   = line[ "begin"   ] as? Int,
+                  let content = line[ "content" ] as? String else {
+                return nil
+            }
+
+            lineMerged += content + "\n"
+        }
+
+        _lyric = lineMerged
+        return lineMerged
     }
 }
