adc6c48e67dec54e0afc67151fd961c26aa1ddb5
namyun1012 <96728184+namyun1012@users.noreply.github.com>
2024-12-10 08:50:59 +0900

    Recommend View Grid화
    Recommend View를 Grid 로 구현해보았습니다.
    - 현재는 임시적으로 내 플레이리스트를 사용하고 있습니다.

--- MusicWith/Main/RecommendView.swift
+++ MusicWith/Main/RecommendView.swift
@@ -10,6 +10,13 @@ import SwiftUI
 struct RecommendView: View {
     @State var playLists: [SpotifyPlayList] = [] // TODO: recommendList 구해야함
     @State var recommend: SpotifyRecommend?
+    @State var me        : SpotifyUser? // State로 두어야 무한 스크롤의 결과가 바로 표시됨
+    @Environment(\.colorScheme) var colorSchema
+
+    let columns = [
+            GridItem(.flexible()),
+            GridItem(.flexible())
+        ]
 
     var body: some View {
         VStack {
@@ -17,44 +24,50 @@ struct RecommendView: View {
                 .font(.title)
                 .padding(.top, 20)
             ScrollView {
-                LazyVStack {
+                LazyVGrid(columns: columns, spacing: 16) {
                     ForEach(playLists, id: \.playListId) { playlist in
                         NavigationLink(destination: PlayListView(playlist: playlist)) {
-                            HStack {
+                            VStack {
                                 AsyncImage(url: URL(string: playlist.imageURL ?? "")) { image in
                                     image
                                         .resizable()
                                         .aspectRatio(contentMode: .fill)
-                                        .frame(width: 50, height: 50)
+                                        .frame(width: 100, height: 100)
                                         .clipped()
                                 } placeholder: {
                                     ProgressView()
-                                        .frame(width: 50, height: 50)
+                                        .frame(width: 100, height: 100)
                                 }
-                                Text(playlist.title ?? "Empty")
-                                    .foregroundColor(.black)
-                                    .padding(.leading, 20)
+                                CustomScrollText(text: playlist.title ?? "Empty")
+                                    .foregroundColor(colorSchema == .dark ? .white : .black)
+                                
                                 Spacer()
                             }
-                            .padding(.vertical, 5)
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color(colorSchema == .dark ? .secondarySystemBackground : .systemBackground))
+                            .cornerRadius(12)
+                            .shadow(radius: 2)
                         }
                         .onAppear {
                             Task {
                                 let lastIndex = playLists.count - 1
-
-                                if playlist.playListId == playLists[ lastIndex ].playListId {
-                                    playLists = await recommend?.playList(idx: 0) ?? []
+                                if playlist.playListId == playLists[lastIndex].playListId {
+                                    playLists = await me?.playList(idx: -1) ?? []
                                 }
                             }
                         }
                     }
-                }
+                }.padding(.horizontal, 2)
             }
             .padding(.horizontal)
         }
         .task {
-            recommend = SpotifyRecommend()
-            playLists = await recommend?.playList(idx: 0) ?? []
+            self.me = SpotifyUser(userId: nil)
+            
+            if let me = me {
+                playLists = await me.playList(idx: -1)
+            }
         }
     }
 }
