dc8ca0f4c9c5120b5eb0fc4cc3afc49645511413
namyun1012 <96728184+namyun1012@users.noreply.github.com>
2024-12-14 10:36:46 +0900

    recommendTest

--- MusicWith/Main/RecommendView.swift
+++ MusicWith/Main/RecommendView.swift
@@ -10,6 +10,11 @@ import Combine
 
 struct RecommendView: View {
     @Environment(\.colorScheme) private var _colorSchema
+    
+    @State private var _playListName:  String  = ""
+    @State private var _trackIds    : [String] = []
+    @State private var _names       : [String: String] = [:]
+    @State private var _imageUrls   : [String: String] = [:]
 
     private let _columns = [
         GridItem(.flexible()),
@@ -21,52 +26,54 @@ struct RecommendView: View {
             Text("현재 인기 있는 음악")
                 .font(.title)
                 .padding(.top, 20)
-            Text("공사중~")
-            // ScrollView {
-            //     LazyVGrid(columns: columns, spacing: 16) {
-            //         ForEach(tracks, id: \.trackId) { song in
-            //             VStack {
-            //                 AsyncImage(url: URL(string: song.imageURL ?? "")) { image in
-            //                     image
-            //                         .resizable()
-            //                         .aspectRatio(contentMode: .fill)
-            //                         .frame(width: 100, height: 100)
-            //                         .clipped()
-            //                 } placeholder: {
-            //                     ProgressView()
-            //                         .frame(width: 100, height: 100)
-            //                 }
-            //                 CustomScrollText(text: song.title ?? "Empty", alignment: .center)
-            //                     .foregroundColor(colorSchema == .dark ? .white : .black)
+             ScrollView {
+                 LazyVGrid(columns: _columns, spacing: 16) {
+                     ForEach(0..<_trackIds.count, id: \.self) { index in
+                         VStack {
+                             AsyncImage(url: URL(string: _imageUrls[ _trackIds[ index ] ] ?? "https://placehold.co/80")) { image in
+                                 image
+                                     .resizable()
+                                     .aspectRatio(contentMode: .fill)
+                                     .frame(width: 100, height: 100)
+                                     .clipped()
+                             } placeholder: {
+                                 ProgressView()
+                                     .frame(width: 100, height: 100)
+                             }
+                             CustomScrollText(text: _names[ _trackIds[ index ] ] ?? "")
+                                 .foregroundColor(_colorSchema == .dark ? .white : .black)
 
-            //                 Spacer()
-            //             }
-            //             .frame(maxWidth: .infinity)
-            //             .padding()
-            //             .background(Color(colorSchema == .dark ? .secondarySystemBackground : .systemBackground))
-            //             .cornerRadius(12)
-            //             .shadow(radius: 2)
-            //             .onTapGesture {
-            //                 Task {
-            //                     await controlState.setSong(song: song)
-            //                 }
-            //             }
-            //             .onAppear {
-            //                 Task {
-            //                     if song.trackId == tracks[tracks.count - 1].trackId {
-            //                         // Infinite Scroll
-            //                     }
-            //                 }
-            //             }
-            //         }
-            //     }.padding(.horizontal, 2)
-            // }
+                             Spacer()
+                         }
+                         .frame(maxWidth: .infinity)
+                         .padding()
+                         .background(Color(_colorSchema == .dark ? .secondarySystemBackground : .systemBackground))
+                         .cornerRadius(12)
+                         .shadow(radius: 2)
+                         .onTapGesture {
+                             TrackPlayer     .shared.setTrack(_trackIds, index)
+                             ControlViewState.shared.showSheet = true
+                         }
+                         .task {
+                             if case nil = _names[ _trackIds[ index ] ] {
+                                 let name     = await Track.name    (_trackIds[ index ])
+                                 let imageUrl = await Track.imageUrl(_trackIds[ index ])
+
+                                 DispatchQueue.main.async {
+                                     _names    [ _trackIds[ index ] ] = name
+                                     _imageUrls[ _trackIds[ index ] ] = imageUrl
+                                 }
+                             }
+                         }
+                     }
+                 }.padding(.horizontal, 2)
+             }
             .padding(.horizontal)
         }
         .task {
-            // test 원할 시 만든 Playlist 중에서 변경
-//            let playlist = SpotifyPlayList(playListId: "4yChwi9z4WjV2ppkIjwaxm")
-//            tracks = await playlist.track(idx: -1)
+            let playListId = "YJ5EdOPU4PN0IMuCOtQVZF7cSMD89qIwCadcbznxSo9grngXZ4d"
+            _playListName = await PlayList.name    (playListId) ?? ""
+            _trackIds     = await PlayList.trackIds(playListId) ?? []
         }
     }
 }
