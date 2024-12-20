c714cd1d3f4fd6f298545fd13047148007165726
kimhappy <kimhappyofficial@gmail.com>
2024-12-20 20:25:46 +0900

    ref: simplify view logic

--- MusicWith/Util/RecommendTrack.swift
+++ MusicWith/Util/RecommendTrack.swift
@@ -4,26 +4,27 @@
 //
 //  Created by user on 12/18/24.
 //
+
 import Foundation
 
 struct RecommendTrack {
-    private static var _list : [String] = []
-    
-    public static func tracks() async -> [String] {
+    private init() {}
+
+    private static var _list: [(String, Int)] = []
+
+    public static func tracks() async -> [(String, Int)]? {
         if !_list.isEmpty {
             return _list
         }
         
-        guard let items = await Query.getMwJson("/hot") as? [[String: Any]] else {
+        guard let items  = await Query.getMwJson("/hot") as? [[String: Any]],
+              let tracks = items.mapOptional({ (item: [String: Any]) -> (String, Int)? in
+                  guard let track_id     = item[ "track_id"     ] as? String,
+                        let num_comments = item[ "num_comments" ] as? Int else { return nil }
+                  return (track_id, num_comments)
+              })
+        else {
             return [];
-        }
-        
-        var tracks : [String] = []
-        
-        for item in items {
-            guard let num_comments = item["num_comments"] as? Int       else {return []}
-            guard let track_id = item["track_id"]         as? String    else {return []}
-            tracks.append(track_id)
         }
         
         _list = tracks
